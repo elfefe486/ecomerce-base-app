e4668a66d4ec499d6d27fbf968f112d435c8f8e8
elfefe486 <fefe1337@gmail.com>
2024-05-06 11:50:44 -0300

    BOTON

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,28 +1,75 @@
 import React from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import db from '@/db/db';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
 
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+db.product.count({ where: { isAvailbleForPurchase: true}}),
+db.product.count({ where: { isAvailbleForPurchase: false}})
+  ])
+  return { activeCount, inactiveCount }
+}
 
 async function getSalesData() {
-  
+ 
     const data = await db.order.aggregate ({
         _sum: {pricePaidInCents: true}, 
         _count: true
      })
-
+     await wait(2000)
      return {
         amount: (data._sum.pricePaidInCents || 0) / 100,
         numberOfSales: data._count
          }
 }
+function wait(duration: number){
+  return new Promise(resolve => setTimeout(resolve, duration))
+}
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+    _sum: { pricePaidInCents: true},
+    }),
+])
+
+return {
+  userCount,
+  averageValuePerUser: userCount === 0 ? 0 : (orderData._sum.
+  pricePaidInCents || 0) / userCount / 100
+  }
+}
+
 
 export default async function AdminDashboard() {
-  const salesData = await getSalesData()
-    return 
-  ( <div className="grid grid-cols-1 md:grid-cols-2 lg: grid-cols-3 gap-4">
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData()
+  ])
+  
+    
+  return (
+   <div className="grid grid-cols-1 md:grid-cols-2 lg: grid-cols-3 gap-4">
 
-<DashboardCard title="sales" subtitle={salesData.numberOfSales} body={salesData.amount}/> 
+<DashboardCard 
+    title="Sales" 
+    subtitle={`${formatNumber(salesData.numberOfSales)} Orders`} 
+    body={formatCurrency(salesData.amount)}
+    /> 
+  <DashboardCard 
+    title="Customers" 
+    subtitle={`${formatCurrency(userData.averageValuePerUser)} Average Value`} 
+    body={formatNumber(userData.userCount)}
+    /> 
+    <DashboardCard 
+    title="Active Products" 
+    subtitle={`${formatNumber(productData.inactiveCount)} Inactive`} 
+    body={formatNumber(productData.activeCount)}
+    /> 
   </div>
+  
   )
 }
 
